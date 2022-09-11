e427d00f7c26a6d8e3afe2b9c19837a5f69e2811
Matej Jellus <juffalow@juffalow.com>
2022-09-11 02:57:23 +0200

    Update menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,18 +6,14 @@ import Nav from 'react-bootstrap/Nav';
 const Menu = () => (
   <Navbar bg="dark" variant="dark" expand="lg">
     <Container fluid>
-      <Navbar.Brand href="#">Tech Wordle</Navbar.Brand>
+      <Navbar.Brand href="/">Tech Wordle</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-        {/*
-          <Nav.Link as={Link} to="/obrazky">Obrazky</Nav.Link>
-          <Nav.Link as={Link} to="/video">Video</Nav.Link>
-          <Nav.Link as={Link} to="/text">Text</Nav.Link>
-        */}
+
         </Nav>
         <Nav>
-          <Nav.Link href="/o-aplikacii">GitHub</Nav.Link>
+          <Nav.Link href="https://github.com/juffalow/tech-wordle">GitHub</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
